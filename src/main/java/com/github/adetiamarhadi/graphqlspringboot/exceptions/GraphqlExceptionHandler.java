d52baa80107917ae15f2b66c58341c80e5e88ce1
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-04-22 11:52:34 +0700

    exception handling with exception handler

---
+++ src/main/java/com/github/adetiamarhadi/graphqlspringboot/exceptions/GraphqlExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.github.adetiamarhadi.graphqlspringboot.exceptions;
+
+import graphql.GraphQLException;
+import graphql.kickstart.spring.error.ThrowableGraphQLError;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Component
+public class GraphqlExceptionHandler {
+
+    @ExceptionHandler(GraphQLException.class)
+    public ThrowableGraphQLError handle(GraphQLException e) {
+        return new ThrowableGraphQLError(e);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ThrowableGraphQLError handle(RuntimeException e) {
+        return new ThrowableGraphQLError(e, "internal server error");
+    }
+}
--- src/main/java/com/github/adetiamarhadi/graphqlspringboot/resolver/bank/ClientResolver.java
+++ src/main/java/com/github/adetiamarhadi/graphqlspringboot/resolver/bank/ClientResolver.java
@@ -2,6 +2,7 @@ package com.github.adetiamarhadi.graphqlspringboot.resolver.bank;
 
 import com.github.adetiamarhadi.graphqlspringboot.domain.bank.BankAccount;
 import com.github.adetiamarhadi.graphqlspringboot.domain.bank.Client;
+import graphql.GraphQLException;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,8 @@ public class ClientResolver implements GraphQLResolver<BankAccount> {
     public Client client(BankAccount bankAccount) {
         log.info("Requesting client data for bank account id {}", bankAccount.getId());
 
-        return Client.builder().id(UUID.randomUUID()).firstName("Adetia").lastName("Marhadi").build();
+        throw new RuntimeException("Spring exception can't connect to database: (sql select *)");
+
+//        return Client.builder().id(UUID.randomUUID()).firstName("Adetia").lastName("Marhadi").build();
     }
 }
