181361e26705fff2c2c6e6d227b539cf8b49a2dc
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-05-03 16:38:03 +0700

    data fetcher result - returning data and errors

--- src/main/java/com/github/adetiamarhadi/graphqlspringboot/resolver/bank/ClientResolver.java
+++ src/main/java/com/github/adetiamarhadi/graphqlspringboot/resolver/bank/ClientResolver.java
@@ -2,7 +2,8 @@ package com.github.adetiamarhadi.graphqlspringboot.resolver.bank;
 
 import com.github.adetiamarhadi.graphqlspringboot.domain.bank.BankAccount;
 import com.github.adetiamarhadi.graphqlspringboot.domain.bank.Client;
-import graphql.GraphQLException;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,11 +14,11 @@ import java.util.UUID;
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public Client client(BankAccount bankAccount) {
+    public DataFetcherResult<Client> client(BankAccount bankAccount) {
         log.info("Requesting client data for bank account id {}", bankAccount.getId());
 
-        throw new RuntimeException("Spring exception can't connect to database: (sql select *)");
-
-//        return Client.builder().id(UUID.randomUUID()).firstName("Adetia").lastName("Marhadi").build();
+        return DataFetcherResult.<Client>newResult()
+                .data(Client.builder().id(UUID.randomUUID()).firstName("Adetia").lastName("Marhadi").build())
+                .error(new GenericGraphQLError("Could not get sub-client id")).build();
     }
 }
