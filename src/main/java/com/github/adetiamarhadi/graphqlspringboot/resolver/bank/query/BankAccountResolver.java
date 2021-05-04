22b38e391baeddacbc5b7ef16dbdfbe993bf2edb
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-05-04 14:47:57 +0700

    selection set

--- src/main/java/com/github/adetiamarhadi/graphqlspringboot/resolver/bank/query/BankAccountResolver.java
+++ src/main/java/com/github/adetiamarhadi/graphqlspringboot/resolver/bank/query/BankAccountResolver.java
@@ -3,17 +3,28 @@ package com.github.adetiamarhadi.graphqlspringboot.resolver.bank.query;
 import com.github.adetiamarhadi.graphqlspringboot.domain.bank.BankAccount;
 import com.github.adetiamarhadi.graphqlspringboot.domain.bank.Currency;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.SelectedField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class BankAccountResolver implements GraphQLQueryResolver {
 
-    public BankAccount bankAccount(UUID id) {
+    public BankAccount bankAccount(UUID id, DataFetchingEnvironment environment) {
         log.info("Retrieving bank account id: {}", id);
+
+        var requestedFields = environment.getSelectionSet().getFields().stream().map(SelectedField::getName)
+                .collect(Collectors.toUnmodifiableSet());
+
+        if (environment.getSelectionSet().contains("specialField")) {
+            // do special stuff
+        }
 
         return BankAccount.builder()
                 .id(id)
